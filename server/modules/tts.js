486ca9c49b338454e4a718d3323cbd10655f97ce
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2020-08-12 12:14:29 +0200

    add queue + reward points

--- server/modules/tts.js
+++ server/modules/tts.js
@@ -1,13 +1,36 @@
 const say = require("say");
 
+const queue = [];
+let saying = false;
+const rewardId = "d4bc9cdb-4d94-43fb-bba2-154c3fc9b1a2";
+
+function sayMessage(message) {
+  if (saying) {
+    return queue.push(message);
+  }
+  saying = true;
+  say.speak(message, null, null, () => {
+    saying = false;
+    if (queue.length) {
+      sayMessage(queue.shift());
+    }
+  });
+}
+
 module.exports = () => ({
   async onMessage({ args }) {
-    const { channel, message } = args;
-    const tag = channel.replace("#", "@");
-    const regexp = new RegExp(`${tag}`, "i");
+    const { message, msg } = args;
+    const cri = msg._tags.get("custom-reward-id");
 
-    if (message.match(regexp)) {
-      say.speak(message.replace(regexp, "on le fait"));
+    if (cri && cri === rewardId) {
+      const nick = msg._tags.get("display-name");
+      sayMessage(`Fab lab on le fait, ${nick} dit, ${message}.`);
     }
+    // const tag = channel.replace("#", "@");
+    // const regexp = new RegExp(`${tag}`, "i");
+    //
+    // if (message.match(regexp)) {
+    //   sayMessage(message.replace(regexp, "on le fait"));
+    // }
   },
 });
