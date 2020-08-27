caf359696d8f72dc59e9997a09c4ee5ca50a400f
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2020-08-27 10:41:17 +0200

    Update clearUserList.js

--- server/modules/clearUserList.js
+++ server/modules/clearUserList.js
@@ -4,9 +4,10 @@ const alias = ["clearUserList", "cul"];
 
 module.exports = () => ({
   async onCommand(args) {
+    const cmd = args.command.name;
     const broadcaster = args.msg._tags.get("badges").includes("broadcaster");
 
-    if (!broadcaster) {
+    if (!broadcaster || !alias.includes(cmd)) {
       return;
     }
 
